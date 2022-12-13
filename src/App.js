16ff6e316390dc5455e5dbaf1fe3ba35268580c2
Jacob Matias <matiasdajacob@gmail.com>
2022-12-12 23:45:32 -0300

    implementacion_navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Componentes/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
